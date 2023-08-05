040776150768c58d144291c2dcb878ff1c770de5
Théo Feschotte <theo@studio22.xyz>
2023-08-05 21:17:52 +0200

    Création du projet

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,31 @@
-// IMPORTS ---------------------------------------------------------------------------
-// ----------------------------------------------------------------------------------------------------
+const startButton = document.getElementsByTagName("button")[0];
+const imgAnimation = Array.from(document.getElementsByTagName("img"));
+let idSetInterval;
+const numSteps = document.getElementById("steps");
 
+startButton.addEventListener("click", function() {
+    if (startButton.innerText === "START" || startButton.innerText === "PLAY") {
+        this.innerText = "Pause";
+        imgAnimation.forEach(img => {
+            img.style.animationPlayState = "running";
+        });
+    } else {
+        this.innerText = "Play";
+        imgAnimation.forEach(img => {
+            img.style.animationPlayState = "paused";
+        });
+    };
+});
+
+numSteps.addEventListener("change", function() {
+    imgAnimation[5].style.animationTimingFunction = "steps("+numSteps.value+")";
+});
+
+numSteps.addEventListener("mousedown", function() {
+    idSetInterval=setInterval(() => {
+        imgAnimation[5].style.animationTimingFunction = "steps("+numSteps.value+")";
+    }, 100);
+});
+numSteps.addEventListener("mouseup", function() {
+    clearInterval(idSetInterval);
+});
